d3d7d19006711da47a617e88abe3ad57cb5a5571
Scott Bromander <scottbromander@gmail.com>
2019-11-26 18:16:23 -0600

    update

--- server/public/scripts/client.js
+++ server/public/scripts/client.js
@@ -3,10 +3,14 @@ $(document).ready(init);
 function init() {
     getSongs();
 
-    $('#new-song').on('submit', postSong);
+    $('#new-song').on('submit', getValuesFromForm);
+
+    $('.container').on('click', '.js-btn-delete', deleteSong);
+    $('.container').on('click', '.js-btn-up', upvote);
+    $('.container').on('click', '.js-btn-down', downvote);
 }
 
-function postSong(event) {
+function getValuesFromForm(event) {
     event.preventDefault();
     const newSong = {
         rank: $('#js-form-rank').val(),
@@ -14,7 +18,10 @@ function postSong(event) {
         track: $('#js-form-track').val(),
         published: $('#js-form-published').val(),
     }
+    postSong(newSong);
+}
 
+function postSong(newSong) {
     $.ajax({
         method: "POST",
         url: "/api/songs",
@@ -41,13 +48,57 @@ function getSongs() {
     })
 }
 
+function deleteSong() {
+    const idNumber = $(this).data('id');
+
+    $.ajax({
+        method: "DELETE",
+        url: '/api/songs/' + idNumber // /api/songs/65
+    })
+    .then((response) => {
+        getSongs();
+    })
+    .catch((response) => {
+        console.warn(response);
+    })
+}
+
+function upvote() {
+    updateSong('up', $(this).data('id'))
+}
+
+function downvote() {
+    updateSong('down', $(this).data('id'));
+}
+
+function updateSong(direction, id) {
+    $.ajax({
+        method: 'PUT',
+        url: '/api/songs/' + id,
+        data:  {
+            direction: direction
+        }
+    })
+    .then((response) => {
+        getSongs();
+    })
+    .catch((err) => {
+        console.warn(err);
+    })
+}
+
 function render(songs) {
     $('.container').empty();
     for(let song of songs) {
         console.log(song);
         $('.container').append(`
             <div>
-                <p>${song.rank} - ${song.artist} - ${song.track}</p>
+                <p>${song.rank} - ${song.artist} - ${song.track} - upvote:
+                <span><button class="js-btn-down" data-id="${song.id}">v</button></span>
+                ${song.upvote}
+                <span><button class="js-btn-up" data-id="${song.id}">^</button></span>
+                <span><button class="js-btn-delete" data-id="${song.id}">X</button</span>
+                </p>
             </div>
         `)
     }
--- server/routers/song.router.js
+++ server/routers/song.router.js
@@ -2,8 +2,8 @@ const express = require('express');
 const router = express.Router();
 const pool = require('../modules/pool');
 
-router.get('/', (req,res) => {
-    pool.query(`SELECT * FROM "songs"`)
+router.get('/', (req, res) => {
+    pool.query(`SELECT * FROM "songs" ORDER BY "id" ASC`)
         .then((response) => {
             res.send(response.rows);
         })
@@ -13,7 +13,7 @@ router.get('/', (req,res) => {
         });
 });
 
-router.post('/', (req,res) => {
+router.post('/', (req, res) => {
     const newSong = req.body;
     console.log(newSong);
 
@@ -30,5 +30,67 @@ router.post('/', (req,res) => {
         });
 });
 
+// Client side, it looks like this -> /api/songs/5
+// Server side, it receives it like this -> /api/songs/:id
+// But now we have a variable, req.params.id = 5
+router.delete('/:id', (req, res) => {
+    console.log(req.params.id);
+    const songId = req.params.id;
+    const queryString = `DELETE FROM "songs" WHERE "id" = ${songId};`;
+
+    pool.query(queryString)
+        .then((response) => {
+            res.sendStatus(200);
+        })
+        .catch((err) => {
+            res.sendStatus(500);
+        })
+});
+
+/*
+    $.ajax({
+        method: 'PUT',
+        url: '/api/songs/' + $(this).data('id')
+        data:  {
+            direction: "up"
+        }
+    })
+*/
+router.put('/:id', (req, res) => {
+    // req.params.id = What entry do you want to update?
+    // req.body.whatever = The data you want to update.
+    const id = req.params.id;
+    const direction = req.body.direction;
+    let updateVote = `upvote + 1`;
+
+    if (direction === "up") {
+        updateVote = `upvote + 1`;
+    } else {
+        updateVote = `upvote - 1`;
+    }
+
+    let queryString = `UPDATE "songs" SET "upvote"=${updateVote} WHERE "id" = $1;`;
+
+    // The value in the array is providing the value for $1 above.
+    pool.query(queryString, [id])
+        .then((response) => {
+            res.sendStatus(200);
+        })
+        .catch((err) => {
+            console.log(err);
+            res.sendStatus(500);
+        })
+});
 
 module.exports = router;
+
+/*
+ $.ajax({
+     method: 'GET',
+     url: '/api/songs/gizmo/baxter'
+ })
+*/
+// router.get('/:kitty/:puppy', (req,res) => {
+    // req.params.kitty = gizmo
+    // req.params.puppy = baxter
+// })
